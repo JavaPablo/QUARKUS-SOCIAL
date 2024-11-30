c58380c79a2075b48aed87c9319ef19d70d92eae
JavaPablo <pablopablo820@gmail.com>
2024-11-30 20:38:07 -0300

    Added bank settings, dependencies and script

---
+++ src/main/java/io/github/pablorabelo/quarkussocial/rest/UserResource.java
@@ -0,0 +1,21 @@
+package io.github.pablorabelo.quarkussocial.rest;
+
+import io.github.pablorabelo.quarkussocial.rest.dto.CreateUserRequest;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+@Path("/users")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+public class UserResource {
+    @POST
+    public Response createUser(CreateUserRequest userRequest) {
+        return Response.ok(userRequest).build();
+    }
+
+    @GET
+    public Response listAllUsers() {
+        return Response.ok().build();
+    }
+}
