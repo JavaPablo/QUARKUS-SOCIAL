3fdb1985a6f68b4bab7597bf4a430db5d15b408d
JavaPablo <pablopablo820@gmail.com>
2024-11-30 21:03:53 -0300

    Creating and listing added users

--- src/main/java/io/github/pablorabelo/quarkussocial/rest/UserResource.java
+++ src/main/java/io/github/pablorabelo/quarkussocial/rest/UserResource.java
@@ -1,6 +1,10 @@
 package io.github.pablorabelo.quarkussocial.rest;
 
+import io.github.pablorabelo.quarkussocial.domain.model.User;
 import io.github.pablorabelo.quarkussocial.rest.dto.CreateUserRequest;
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+import io.quarkus.hibernate.orm.panache.PanacheQuery;
+import jakarta.transaction.Transactional;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
@@ -10,12 +14,18 @@ import jakarta.ws.rs.core.Response;
 @Produces(MediaType.APPLICATION_JSON)
 public class UserResource {
     @POST
+    @Transactional
     public Response createUser(CreateUserRequest userRequest) {
-        return Response.ok(userRequest).build();
+        User user = new User();
+        user.setAge(userRequest.getAge());
+        user.setName(userRequest.getName());
+        user.persist();
+        return Response.ok(user).build();
     }
 
     @GET
     public Response listAllUsers() {
-        return Response.ok().build();
+        PanacheQuery<PanacheEntityBase> query = User.findAll();
+        return Response.ok(query.list()).build();
     }
 }
